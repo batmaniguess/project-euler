c43b3cce64aab7a160a80187b3f86f3f1f8ef5ec
batmaniguess <sanjithk50201@gmail.com>
2025-01-24 00:46:04 +0530

    largest prime factor, easy

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@
 * */
 
 fn mutliple_three_five(bounds: u32, a: u32, b: u32) -> u32 {
-    let mut total: u32 = 0;
+    let mut total = 0;
     for i in 0..bounds {
         if i % a == 0 || i % b == 0 {
             total += i;
         }
     }
-    return total;
+    total
 }
 
 fn even_fibonacci(bounds: u32) -> u32 {
-    let mut total: u32 = 0;
-    let mut a: u32 = 1;
-    let mut b: u32 = 2;
+    let mut total = 0;
+    let mut a = 1;
+    let mut b = 2;
 
     while a <= bounds {
         if a % 2 == 0 {
@@ -26,10 +26,25 @@ fn even_fibonacci(bounds: u32) -> u32 {
         b = next;
     }
 
-    return total;
+    total
+}
+
+fn largest_prime_factor(mut n: u64) -> u64 {
+    let mut factor = 2;
+    let mut largest = 0;
+
+    while n > 1 {
+        while n % factor == 0 {
+            largest = factor;
+            n /= factor;
+        }
+        factor += 1;
+    }
+    largest
 }
 
 fn main() {
     println!("{}", mutliple_three_five(1000, 3, 5));
     println!("{}", even_fibonacci(4000000));
+    println!("{}", largest_prime_factor(600851475143));
 }
